395dc0690b4ca0b3473ada76b86d97f8d6e66307
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2016-12-31 19:21:55 +0000

    Change production log_level and add last_updated_at timestamp in API response

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ReportsController < ApplicationController
     def index
+        @reports = Report.latest
         render :json => \
             {
-                data: Report.latest,
-                created_by: "Zulhilmi Zainudin",
-                github:     "https://github.com/zulhfreelancer/banjir-api",
-                source:     "http://bencanaalam.jkr.gov.my/v2/"
+                data:            @reports,
+                last_updated_at: @reports.last.created_at,
+                created_by:      "Zulhilmi Zainudin",
+                github:          "https://github.com/zulhfreelancer/banjir-api",
+                source:          "http://bencanaalam.jkr.gov.my/v2/"
             },
             except: [:created_at, :updated_at, :batch_id]
     end
