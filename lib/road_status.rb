9de9803fd292e23a28d83c143fdbbdffbb8d3be0
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2017-01-01 08:01:30 +0000

    Rescue include? method when chained with nil object

--- lib/road_status.rb
+++ lib/road_status.rb
@@ -13,16 +13,14 @@ class RoadStatus
     def self.get_status(page)
         recent  = get_recent(page)
         history = get_history(page)
-        return recent if recent && recent.include?("kenderaan")   
-        history.include?("kenderaan") ? history : nil
+        recent.present? ? recent : history
     end
 
     # previous record / not most recent / top in table UI
     def self.get_history(page)
         begin
-            row    = page.css('.print table table tr:nth-child(2) td:nth-child(2)')
-            # remove leading and ending empty spaces from it
-            Sanitize.fragment(row).squish.split("-")[1].squish    
+            row = page.css('.print table table tr:nth-child(2) td:nth-child(2)')
+            extract_cell(row) 
         rescue Exception
             return nil
         end
@@ -31,12 +29,18 @@ class RoadStatus
     # most recent / bottom in table UI
     def self.get_recent(page)
         begin
-            row     = page.css('.print table table tr:nth-child(3) td:nth-child(2)')
-            # remove leading and ending empty spaces from it
-            Sanitize.fragment(row).squish.split("-")[1].squish
+            row = page.css('.print table table tr:nth-child(3) td:nth-child(2)')
+            extract_cell(row)
         rescue Exception
             return nil
         end
+    end
+
+    # extract table td and remove leading and ending empty spaces from it
+    # return the first line if the line contains "kenderaan" in it
+    def self.extract_cell(row)
+        status = Sanitize.fragment(row).squish.split("-")[1].squish
+        status.include?("kenderaan") ? status : nil
     end
 
 end
