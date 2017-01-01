bebd340924dcd9009ad36512cb4cbe1ef6cb31e0
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2017-01-01 22:15:21 +0000

    Re-enable the autoload functionality for all environments

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ module BanjirApi
   class Application < Rails::Application
     config.time_zone = 'Asia/Kuala_Lumpur'
     config.autoload_paths << Rails.root.join('lib')
+    config.enable_dependency_loading = true
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
